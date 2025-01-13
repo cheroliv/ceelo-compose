f5566845f88c328f01d0ee4f2148e1fecd8d6a3c
cheroliv <cheroliv@laposte.net>
2025-01-14 00:26:53 +0100

    feat: Rename root project and update build configurations
    - Renames the root project from "CeeloCompose" to "ceelo-compose".
    - Updates `app/build.gradle.kts` to set `kotlinOptions.jvmTarget` and `buildFeatures.compose` properties directly instead of through a closure.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "0.0.1"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -35,16 +34,13 @@ android {
         sourceCompatibility = VERSION_11
         targetCompatibility = VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = VERSION_11.majorVersion
-    }
-    buildFeatures {
-        compose = true
-    }
+    kotlinOptions.jvmTarget = VERSION_11.majorVersion
+
+    buildFeatures.compose = true
+
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -62,5 +58,4 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlin.test.junit)
-
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "CeeloCompose"
+rootProject.name = "ceelo-compose"
 include(":app")
